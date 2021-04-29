dff0faeb46e3a24a2bab246c30c2ab1ccfcf4c0e
andre <gombkaa@gmail.com>
2021-04-29 11:50:30 +0200

    Configure packages

--- TribalWars/src/WebApi/Startup.cs
+++ TribalWars/src/WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,11 +28,37 @@ namespace WebApi
                 options.AddDefaultPolicy(
                     builder => { builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
             });
-
+            
+            services.AddSignalR();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "tribal wars", Version = "v1"});
+
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer"
+                    });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
 
