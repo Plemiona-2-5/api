c8d9ffad589bbe2f962e39a14355917a44ca0d52
Skrabka98 <56127541+Skrabka98@users.noreply.github.com>
2021-05-05 11:54:30 +0200

    Required buildings

---
+++ TribalWars/src/ApplicationCore/Services/BuildingRequiredService.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class BuildingRequiredService : IBuildingRequiredService
+    {
+        private readonly IBuildingRequiredRepository _buildingRequiredRepository;
+        public BuildingRequiredService(IBuildingRequiredRepository buildingRequiredRepository)
+        {
+            _buildingRequiredRepository = buildingRequiredRepository;
+        }
+        public async Task<IEnumerable<BuildingRequiredBuilding>> RequiredBuilding(int id)
+        {
+            return await _buildingRequiredRepository.RequiredBuilding(id);
+        }
+    }
+}
---
+++ TribalWars/src/Infrastructure/Repository/BuildingRequiredRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class BuildingRequiredRepository : BaseRepository, IBuildingRequiredRepository
+    {
+        public BuildingRequiredRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+        public async Task<IEnumerable<BuildingRequiredBuilding>> RequiredBuilding(int id)
+        {
+            var building = _context.BuildingRequiredBuildings.Where(building => building.BuildingId == id);
+            return building;
+        }
+    }
+}
