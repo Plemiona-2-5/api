18ff0ef6abdc42374ea17df02af72e23fba73a08
Skrabka98 <56127541+Skrabka98@users.noreply.github.com>
2021-05-12 17:05:28 +0200

    Initial

---
+++ TribalWars/src/WebApi/Controllers/RequiredBuildingsController.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class RequiredBuildingsController : Controller
+    {
+        private readonly IBuildingRequiredService _buildingRequiredService;
+
+        public RequiredBuildingsController(IBuildingRequiredService buildingRequiredService)
+        {
+            _buildingRequiredService = buildingRequiredService;
+        }
+
+        [HttpGet("api/v1/getRequiredBuildings")]
+        public IEnumerable<BuildingRequiredBuilding> GetRequiredBuildings(int buildingId)
+        {
+            return _buildingRequiredService.GetRequiredBuildings(buildingId);
+        }
+    }
+}
