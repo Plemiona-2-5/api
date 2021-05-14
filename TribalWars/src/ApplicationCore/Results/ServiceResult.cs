d434f81ac6c5f588b9cd9cdd85c1b3bd174c5c7d
andre <gombkaa@gmail.com>
2021-05-14 08:28:41 +0200

    Add enum to ServiceResult as a result status

--- TribalWars/src/ApplicationCore/Results/ServiceResult.cs
+++ TribalWars/src/ApplicationCore/Results/ServiceResult.cs
@@ -1,31 +1,34 @@
 using System.Collections.Generic;
+using ApplicationCore.Enums;
 
 namespace ApplicationCore.Results
 {
     public class ServiceResult
     {
         public IEnumerable<string> Errors { get; }
-        public bool Succeeded { get; }
+        public ServiceResultStatus ServiceStatus { get; }
 
-        public ServiceResult(bool succeeded, IEnumerable<string> errors)
+        public ServiceResult(ServiceResultStatus serviceStatus, IEnumerable<string> errors)
         {
-            Succeeded = succeeded;
+            ServiceStatus = serviceStatus;
             Errors = errors;
         }
 
+        public bool Succeeded => ServiceStatus == ServiceResultStatus.Success;
+
         public static ServiceResult Success()
         {
-            return new(true, new string[] { });
+            return new(ServiceResultStatus.Success, new string[] { });
         }
 
         public static ServiceResult Failure(IEnumerable<string> errors)
         {
-            return new(false, errors);
+            return new(ServiceResultStatus.Failure, errors);
         }
 
         public static ServiceResult Failure(string error)
         {
-            return new(false, new List<string> {error});
+            return new(ServiceResultStatus.Failure, new List<string> {error});
         }
     }
 }
