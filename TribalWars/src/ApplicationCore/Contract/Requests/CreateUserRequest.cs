8704b3301dee72d8f3568106260705ad64936269
andre <gombkaa@gmail.com>
2021-05-18 08:45:12 +0200

    Add requests data validation with DataAnnotation

--- TribalWars/src/ApplicationCore/Contract/Requests/CreateUserRequest.cs
+++ TribalWars/src/ApplicationCore/Contract/Requests/CreateUserRequest.cs
@@ -1,10 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApplicationCore.Contract.Requests
 {
     public class CreateUserRequest
     {
-        public string Email { get; set; }        
+        [Required] 
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "The {0} must be between {1} and {2} characters length")]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(18, MinimumLength = 8, ErrorMessage = "The {0} must be between {1} and {2} characters length")]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$",
+            ErrorMessage = "The {0} must contains small, big and alphanumeric letters")]
         public string Password { get; set; }
+
+        [Required]
+        [StringLength(18, MinimumLength = 8, ErrorMessage = "The {0} must be between {1} and {2} characters length")]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$",
+            ErrorMessage = "The {0} must contains small, big and alphanumeric letters")]
+        [Compare("Password")]
         public string ConfirmedPassword { get; set; }
     }
 }
--- TribalWars/src/ApplicationCore/Contract/Requests/LoginRequest.cs
+++ TribalWars/src/ApplicationCore/Contract/Requests/LoginRequest.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApplicationCore.Contract.Requests
 {
     public class LoginRequest
     {
+        [Required] 
+        [EmailAddress] 
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(18, MinimumLength = 8, ErrorMessage = "The {0} must be between {1} and {2} characters length")]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$",
+            ErrorMessage = "The {0} must contains small, big and alphanumeric letters")]
         public string Password { get; set; }
     }
 }
