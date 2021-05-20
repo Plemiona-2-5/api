064883a6dd2a8c22b753f6b8ef9b08b804a661b1
Skrabka98 <56127541+Skrabka98@users.noreply.github.com>
2021-05-20 19:19:02 +0200

    Remove unused usings

--- TribalWars/src/Infrastructure/Repository/TribeUserRepository.cs
+++ TribalWars/src/Infrastructure/Repository/TribeUserRepository.cs
@@ -2,10 +2,8 @@
 using ApplicationCore.Interfaces.Repository;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
