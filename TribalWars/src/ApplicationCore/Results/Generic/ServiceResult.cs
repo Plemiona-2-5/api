855e41f8e9f8db14f4baba3f1cd527292f66653a
Bartosz Skrabka <56127541+Skrabka98@users.noreply.github.com>
2021-05-22 00:50:15 +0200

    edit bug in description, get details (#53)

--- TribalWars/src/ApplicationCore/Results/Generic/ServiceResult.cs
+++ TribalWars/src/ApplicationCore/Results/Generic/ServiceResult.cs
@@ -1,14 +1,20 @@
 ﻿using ApplicationCore.Enums;
-using ApplicationCore.Results;
 using System.Collections.Generic;
 
 namespace ApplicationCore.Results.Generic
 {
     public class ServiceResult<T> : ServiceResult
     {
-        public ServiceResult(ServiceResultStatus serviceStatus, IEnumerable<string> errors) : base(serviceStatus, errors)
+        public T Content { get; }
+
+        public ServiceResult(ServiceResultStatus serviceStatus, IEnumerable<string> errors, T content) : base(serviceStatus, errors)
         {
+            Content = content;
         }
-        public T Content { get; set; }
+
+        public static ServiceResult<T> Success(T content)
+        {
+            return new(ServiceResultStatus.Success, null, content );
+        }
     }
 }
