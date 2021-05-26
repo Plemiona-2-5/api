3613086e3196ced4c9b2d4857304e5c837970b98
Skrabka98 <56127541+Skrabka98@users.noreply.github.com>
2021-05-26 13:37:20 +0200

    Remove usings

--- TribalWars/src/ApplicationCore/Interfaces/Services/IVillageService.cs
+++ TribalWars/src/ApplicationCore/Interfaces/Services/IVillageService.cs
@@ -1,8 +1,5 @@
 ﻿using ApplicationCore.Results;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ApplicationCore.Interfaces.Services
--- TribalWars/src/WebApi/Controllers/VillageController.cs
+++ TribalWars/src/WebApi/Controllers/VillageController.cs
@@ -2,8 +2,6 @@
 using ApplicationCore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
