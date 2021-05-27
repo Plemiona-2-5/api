3a7c656649a237e115537a0bfc9139edbfd1739b
andre <gombkaa@gmail.com>
2021-05-27 06:15:51 +0200

    Add string localizer to error messages

--- TribalWars/src/WebApi/Controllers/LoginController.cs
+++ TribalWars/src/WebApi/Controllers/LoginController.cs
@@ -2,7 +2,9 @@ using System.Threading.Tasks;
 using ApplicationCore.Contract.Requests;
 using ApplicationCore.Contract.Responses;
 using ApplicationCore.Interfaces;
+using ApplicationCore.Resources;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace WebApi.Controllers
 {
@@ -11,11 +13,14 @@ namespace WebApi.Controllers
     {
         private readonly IUserService _userService;
         private readonly IJwtService _jwtService;
+        private readonly IStringLocalizer<MessageResource> _localizer;
 
-        public LoginController(IUserService userService, IJwtService jwtService)
+        public LoginController(IUserService userService, IJwtService jwtService,
+            IStringLocalizer<MessageResource> localizer)
         {
             _userService = userService;
             _jwtService = jwtService;
+            _localizer = localizer;
         }
 
         [HttpPost("api/auth/login")]
@@ -24,14 +29,14 @@ namespace WebApi.Controllers
             var userDto = await _userService.GetUserDtoByCredentialsAsync(request.Email, request.Password);
 
             if (userDto == null)
-                return BadRequest(new ErrorsResponse("login or password are wrong"));
+                return BadRequest(new ErrorsResponse(_localizer["WrongPasswordOrLogin"]));
 
             if (!userDto.EmailConfirmed)
-                return BadRequest(new ErrorsResponse("email isn't confirmed"));
+                return BadRequest(new ErrorsResponse(_localizer["NotConfirmedEmail"]));
 
             var accessToken = _jwtService.GenerateJwtToken(userDto.Id.ToString());
             var response = new LoginResponse {AccessToken = accessToken, UserName = userDto.UserName};
-            
+
             return Ok(response);
         }
     }
--- TribalWars/src/WebApi/Controllers/RegisterController.cs
+++ TribalWars/src/WebApi/Controllers/RegisterController.cs
@@ -3,8 +3,10 @@ using ApplicationCore.Contract.Requests;
 using ApplicationCore.Contract.Responses;
 using ApplicationCore.DTOs;
 using ApplicationCore.Interfaces;
+using ApplicationCore.Resources;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace WebApi.Controllers
 {
@@ -14,39 +16,41 @@ namespace WebApi.Controllers
     {
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
+        private readonly IStringLocalizer<MessageResource> _localizer;
 
-        public RegisterController(IUserService userService, IMapper mapper)
+        public RegisterController(IUserService userService, IMapper mapper, IStringLocalizer<MessageResource> localizer)
         {
             _userService = userService;
             _mapper = mapper;
+            _localizer = localizer;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request)
         {
             if (await _userService.UserExistsByEmailAsync(request.Email))
-                return BadRequest(new ErrorsResponse("user with this email already exists"));
+                return BadRequest(new ErrorsResponse(_localizer["EmailIsTaken"]));
 
             if (await _userService.UserExistsByUserNameAsync(request.UserName))
-                return BadRequest(new ErrorsResponse("user with this user name already exists"));
+                return BadRequest(new ErrorsResponse(_localizer["UsernameIsTaken"]));
 
             var userDto = _mapper.Map<UserDto>(request);
-            
+
             var (result, emailConfirmationToken) = await _userService.CreateUserAsync(userDto, request.Password);
 
-             return result.Succeeded
-                 ? Ok(new {EmailConfirmationToken = emailConfirmationToken})
-                 : BadRequest(new ErrorsResponse(result.Errors));
+            return result.Succeeded
+                ? Ok(new {EmailConfirmationToken = emailConfirmationToken})
+                : BadRequest(new ErrorsResponse(result.Errors));
         }
 
         [HttpPost("confirm-email")]
         public async Task<IActionResult> ConfirmEmail([FromBody] ConfirmEmailRequest request)
         {
             if (!await _userService.UserExistsByEmailAsync(request.Email))
-                return BadRequest(new ErrorsResponse("user with this email doesn't exist"));
+                return BadRequest(new ErrorsResponse(_localizer["UserNotFoundByEmail"]));
 
             if (await _userService.EmailConfirmedAsync(request.Email))
-                return BadRequest(new ErrorsResponse("user email is already confirmed"));
+                return BadRequest(new ErrorsResponse(_localizer["EmailAlreadyConfirmed"]));
 
             var result = await _userService.ConfirmUserEmailAsync(request.Email, request.EmailConfirmationToken);
 
